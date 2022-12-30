44fee2f209d5a54af2f601a89c9138863025ba13
tquyen2109 <t.quyen1997@yahoo.com>
2022-12-30 14:46:52 +0700

    Create local instance of contract

---
+++ src/lottery.js
@@ -0,0 +1,48 @@
+import web3 from "./web3";
+
+const address = '0x3074Adb75eDCc20997c695bd6020292C15622ea2';
+
+const abi = [{
+    "constant": true,
+    "inputs": [],
+    "name": "manager",
+    "outputs": [{"name": "", "type": "address"}],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+}, {
+    "constant": false,
+    "inputs": [],
+    "name": "pickWinner",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+}, {
+    "constant": true,
+    "inputs": [],
+    "name": "getPlayers",
+    "outputs": [{"name": "", "type": "address[]"}],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+}, {
+    "constant": false,
+    "inputs": [],
+    "name": "enter",
+    "outputs": [],
+    "payable": true,
+    "stateMutability": "payable",
+    "type": "function"
+}, {
+    "constant": true,
+    "inputs": [{"name": "", "type": "uint256"}],
+    "name": "players",
+    "outputs": [{"name": "", "type": "address"}],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+}, {"inputs": [], "payable": false, "stateMutability": "nonpayable", "type": "constructor"}];
+
+//Create local instance of the contract
+export default new web3.eth.Contract(abi, address);
